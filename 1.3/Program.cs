f3fca95b981e69c226c204fa9d005ba61b2747da
TemaOOOOOO <TemaOOOOOO@DESKTOP-AMG6T8H>
2024-03-05 19:47:26 +0200

    Добавьте файлы проекта.

---
+++ 1.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
+        var result = numbers.Where(x => x < 0 && x % 2 == 0).Reverse();
+        Console.WriteLine("Парные отрицательные числа в обратном порядке:");
+        foreach (var num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
